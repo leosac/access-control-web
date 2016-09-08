fb1132822cba4ffa53751f4ca56d8818c520bd1b
xaqq <kapp.arno@gmail.com>
2016-09-08 19:19:05 +0200

    improve password change, now use component

---
+++ app/components/password-change.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+import {validator, buildValidations} from 'ember-cp-validations';
+
+// Validation for password change
+const Validations = buildValidations({
+    current_password: validator('presence',
+        {
+            isWarning: true,
+            presence: true,
+            ignoreBlank: false,
+            message: "Shouldn't be left blank, unless you are an administrator."
+        }),
+    new_password: validator('presence', true),
+    new_password2: validator('confirmation', {
+        on: 'new_password',
+        message: 'Does not match the new password',
+    })
+});
+
+export default Ember.Component.extend(Validations, {
+    passwordChange: Ember.inject.service('password-change'),
+    user_id: null,
+    current_password: null,
+    new_password: null,
+    new_password2: null,
+
+    actions: {
+        changePassword: function ()
+        {
+            const fm = this.get('flashMessages');
+
+            this.get('passwordChange').changePassword(this.get('user_id'),
+                this.get('current_password'),
+                this.get('new_password')).then(() =>
+            {
+                fm.success('Password successfully changed.');
+            });
+        },
+    }
+});
--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,30 +1,5 @@
 import Ember from 'ember';
 import LeosacRoute from '../leosac-route';
-import {validator, buildValidations} from 'ember-cp-validations';
-
-// Validation for password change
-const Validations = buildValidations({
-    current_password: validator('length',
-        {
-            isWarning: true,
-            //presence: true,
-            min: 6,
-            ignoreBlank: false,
-            message: "Shouldn't be left blank, unless you are an administrator."
-        }),
-    new_password: validator('presence', true),
-    new_password2: validator('confirmation', {
-        on: 'new_password',
-        message: 'Does not match the new password',
-    })
-});
-
-// Ember Object for password change
-const passwordInfoTemplate = Ember.Object.extend(Validations, {
-    current_password: null,
-    new_password: null,
-    new_password2: null,
-});
 
 export default LeosacRoute.extend({
     passwordChange: Ember.inject.service('password-change'),
@@ -40,12 +15,8 @@ export default LeosacRoute.extend({
     model(params)
     {
         "use strict";
-        this.set('passwordInfo', passwordInfoTemplate.create(
-            Ember.getOwner(this).ownerInjection(), {}));
-
         return Ember.RSVP.hash({
             user: this.get('store').findRecord('user', params.user_id),
-            passwordInfo: this.get('passwordInfo')
         });
     },
     actions: {
@@ -59,28 +30,6 @@ export default LeosacRoute.extend({
             }, (why) =>
             {
                 fm.danger('Failed to update profile: ' + why.status_string);
-            });
-        },
-        changePassword: function ()
-        {
-            const passwordInfo = this.controller.get('model').passwordInfo;
-            const fm = this.get('flashMessages');
-            const uid = this.paramsFor('profile').user_id;
-
-            const self = this;
-            this.get('passwordChange').changePassword(uid,
-                passwordInfo.current_password,
-                passwordInfo.new_password).then(() =>
-            {
-                fm.success('Password successfully changed.');
-                //self.controller.transitionToRoute('profile', uid + 1);
-                Ember.run.later(() =>
-                {
-                    self.refresh();
-                }, 3000);
-                //self.transitionTo('profile', uid);
-                //self.controller.set('model.passwordInfo', passwordInfoTemplate.create(
-                //Ember.getOwner(this).ownerInjection(),{}));
             });
         },
         refreshLama: function ()
