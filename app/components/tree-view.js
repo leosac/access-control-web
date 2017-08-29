13540d9c21b842df718f0f11096afc30be848a86
marin <marin.brunel@epitech.eu>
2017-08-29 17:12:56 +0200

    Added the icon to the tree, separated by type(logical and physical). Implemanted the types plugins, with his options, like the icon. Added a beginning of function that will permit to understand more the drag and drop action. Aerated the code in the file too.

--- app/components/tree-view.js
+++ app/components/tree-view.js
@@ -11,25 +11,24 @@ function addNode(root, child) {
     console.log(child.text + " correctly added as child to " + root.text);
 }
 
-function addChildren(child) {
-    if (child.length)
-        return {
-        };
-    else
-        return {
-            child
-        }
-}
-
 function nodeFromZone(zone)
 {
+    let type;
+    if (zone.get('type') === 'zone.type.logical')
+        type = 'logicalZone';
+    else
+        type = 'physicalZone';
     return {
         id: zone.get('id'),
         text: zone.get('alias'),
-        children : [
-            //addChildren(zone.get('children'))
-        ]
+        children : [],
+        type: type
     };
+}
+
+function checkType(node, model) {
+    console.log(model);
+    console.log(node);
 }
 
 function getNodeFromCache(zone, cache)
@@ -48,25 +47,49 @@ function getNodeFromCache(zone, cache)
 }
 
 export default Ember.Component.extend({
-    "plugins" : "types, unique, dnd",
-    "types": {
-        "physical": {
-            "icon": "glyphicon glyphicon-tag"
+    "plugins" : "types, dnd, contextmenu",
+
+    typesOptions: {
+        "physical" : {
+            "icon" : "fa fa-building"
         },
-        "logical": {
+        "logical" : {
+            "icon" : "fa fa-users"
+        },
+        "root": {
+            "icon" : "fa fa-cube"
+        },
+        "physicalZone": {
             "icon": "fa fa-building-o"
+        },
+        "logicalZone": {
+            "icon": "fa fa-user"
         }
     },
-    'jstreeActionReceiver': true,
+
+    'jsTreeActionReceiver': true,
 
     zoneDataTree: Ember.computed('model', function(){
+        console.log(this.get('model'));
         console.log("ENTERING TREE-VIEW");
-        let physicalZoneNode = {'id': 'physicalRoot', "type": "physical",'text': 'physicalRoot', 'children': []};
-        let logicalZoneNode = {'id': 'logicalRoot', "type": "logical",'text': 'logicalRoot', 'children': []};
+        let physicalZoneNode = {
+            'id': 'physicalRoot',
+            'type': 'physical',
+            'text': 'physicalRoot',
+            'children': []};
+        let logicalZoneNode = {
+            'id': 'logicalRoot',
+            "type": "logical",
+            'text': 'logicalRoot',
+            'children': []};
         let nodeCache = {};
         let i = 0;
 
-        let tree = {'text': 'Root', 'id': 'Root','children': []};
+        let tree = {
+            'text': 'Root',
+            'id': 'Root',
+            'type': 'root',
+            'children': []};
         this.get('model').forEach(function(zone){
             const n = getNodeFromCache(zone, nodeCache);
             console.log("Zone loop number " + i++);
@@ -97,16 +120,42 @@ export default Ember.Component.extend({
         console.log("EXITING TREE-VIEW");
         return tree;
     }),
+
     actions:
         {
+            saveTree() {
+              this.get('model').save();
+            },
+           handleJstreeEventDidMoveNode(node) {
+                //console.log(node.parent);
+                let j = 0;
+                let idParent = -1;
+                this.get('model').forEach(function(zone) {
+                    zone.get('parent').forEach(function(parent) {
+                        if (node.parent === parent.get('id'))
+                            idParent = j;
+                        j++;
+                    });
+                    if (idParent !== -1) {
+                        console.log("TYUIOUYTUIOUI");
+                        console.log(zone.get('parent')[idParent]);
+                    }
+                });
+
+               checkType(node, this.get('model'));
+               this.get('jsTreeActionReceiver').send('moveNode');
+           },
             expandAll() {
                 this.get('jsTreeActionReceiver').send('openAll');
             },
             collapseAll() {
                 this.get('jsTreeActionReceiver').send('closeAll');
             },
-            handleJstreeEventDidChange(node) {
-               // this.get('jsTreeActionReceiver').send('getNode', node);
+            handleJstreeEventDidSelectNode(node) {
+                this.get('jsTreeActionReceiver').send('getNode', node.id);
+            },
+            handleJstreeGetNode(node) {
+                console.log(node);
             },
             closeAllNodes() {
                 this.get('jsTreeActionReceiver').send('closeAll');
