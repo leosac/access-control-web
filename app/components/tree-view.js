5df28b4f2e76c88bdd56c1c69fa227fcc1621074
marin <marin.brunel@epitech.eu>
2017-08-31 10:53:37 +0200

    test

--- app/components/tree-view.js
+++ app/components/tree-view.js
@@ -60,7 +60,6 @@ export default Ember.Component.extend({
 
     typesOptions: {
         "physical": {
-            draggable : false,
             "icon": "fa fa-building"
         },
         "logical": {
@@ -131,12 +130,39 @@ export default Ember.Component.extend({
         console.log("EXITING TREE-VIEW");
         return tree;
     }),
-    checkCallback(operation, node, node_parent, node_position, more)
-    {
-        // faire des consoles.log
-        // si jamais
-        console.log("Je suis dedans");
-        return operation !== 'move_node';
+    checkCallback(operation, node, node_parent, node_position, more) {
+
+        //check if we try to move a node
+        if (operation === 'move_node') {
+            // check if the node is a node that we created otherwise it can't be moved
+            if (node.type === 'logicalZone' ||
+                node.type === 'physicalZone')
+            {
+                // If a node is of type logical,
+                // she can either go in the logical part of the tree,
+                // or in the physical zone that we created
+                // For example:
+                // the logical zone "admin_group" can be located in the physical zone "admin_room"
+                if (node.type === 'logicalZone')
+                {
+                    if (node_parent.type === 'logicalZone' ||
+                        node_parent.type === 'logical')
+                        return true;
+                }
+                // but if a zone is of type physical, she can only go in the physical zone
+                // If we take the example above, the "admin_room" can only be part of the "floor" or "building" physical zone,
+                // there is no point in allowing him to be part of a logical zone
+                else
+                {
+                    if (node_parent.type === 'physicalZone' ||
+                        node_parent.type === 'physical' ||
+                        node_parent.type === 'logicalZone')
+                        return true;
+                }
+            }
+            return false;
+        }
+        return true;
     },
 
     actions:
@@ -171,9 +197,35 @@ export default Ember.Component.extend({
                     allow += 1;
                 if ((newParent = nodeFromId(node.parent, this.get('model'))) === -1)
                     allow += 2;
-                if ((currentZone = nodeFromId(node.node.id, this.get('model'))) === -1)
-                    allow = 2;
 
+                currentZone = nodeFromId(node.node.id, this.get('model'));
+
+                // if the oldParent is illegal, like he is root-like
+                if  (allow === 1)
+                {
+                    currentZone.get('parent').pushObject(newParent);
+                    newParent.save().then(() => {
+                            this.get('flashMessages').success('Zone successfully edited.');
+                        },
+                        () => {
+                            this.get('flashMessages').danger('An error occurred while editing zone');
+
+                        });
+                }
+                // if the newParent is illegal, like he is root-like
+                else if (allow === 2)
+                {
+                    currentZone.get('parent').removeObject(oldParent);
+                    oldParent.save().then(() => {
+                            this.get('flashMessages').success('Zone successfully edited.');
+                        },
+                        () => {
+                            this.get('flashMessages').danger('An error occurred while editing zone');
+                        });
+                }
+                // if both parent are legal
+                else if (allow === 0)
+                {
                     currentZone.get('parent').pushObject(newParent);
                     currentZone.get('parent').removeObject(oldParent);
                     oldParent.save().then(() => {
@@ -188,8 +240,12 @@ export default Ember.Component.extend({
                         () => {
                             this.get('flashMessages').danger('An error occurred while editing zone');
                         });
+                }
 
-                    console.log("The zone can be moved");
+                // the last case is when both parent are illegal,
+                // but in this case, with the checkback notification,
+                // it means that the zone doesn't move, so it should be ignored
+                console.log("The zone can be moved");
                 this.get('jsTreeActionReceiver').send('moveNode');
             }
         }
