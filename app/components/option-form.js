6038ff3f6c22134e03dd4e3a318ff4ad419795d0
marin <marin.brunel@epitech.eu>
2017-09-12 17:26:13 +0200

    Added file to implement the ember-upload addon, which will we permit us to properly load a logo to our app to customize it. We can now properly set the language in the settings

--- app/components/option-form.js
+++ app/components/option-form.js
@@ -3,14 +3,29 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     authSrv: Ember.inject.service('authentication'),
     leosacInfo: Ember.inject.service('leosac-info'),
+    websocket: Ember.inject.service('websocket'),
+    store: Ember.inject.service(),
     _title: 'settings',
     /**
      * onLogout action name.
      */
     onLogout: 'onLogout',
     languageOption: ['Français', 'English'],
+    language: '',
+    newName: null,
+    init() {
+        this._super(...arguments);
+        let loc = this.get('leosacInfo').getLocale();
+        if (loc === 'fr')
+            this.set('language', 'Français');
+        else
+            this.set('language', 'English');
+    },
 
     actions: {
+        restart() {
+            this.get('leosacInfo').restart();
+        },
         logout()
         {
             "use strict";
@@ -20,9 +35,36 @@ export default Ember.Component.extend({
                 self.sendAction('onLogout');
             });
         },
+        getNameApp()
+        {
+            console.log(this.get('leosacInfo').getNameApp());
+        },
+        setNameApp()
+        {
+            let name = this.get('newName');
+            this.get('leosacInfo').setNameApp(name);
+        },
+        getLogoPath() {
+            console.log(this.get('leosacInfo').getLogoPath());
+        },
+        setLogoPath() {
+            let path = this.get('newPath');
+            this.get('leosacInfo').setLogoPath(path);
+        },
+        changeLanguage(lang)
+        {
+            this.set('language', lang);
+        },
+        getLocale() {
+            if (this.get('leosacInfo').getLocale() === 'fr')
+                return 'Français';
+            else
+                return 'English';
+        },
+        // It help the ember-power-select component to change the value of the locale variable,
+        // then refresh it because we need it
         setLocale(loc)
         {
-            console.log(loc);
             if (loc === 'Français')
                 this.get('leosacInfo').setLocale('fr');
             else
--- app/routes/settings.js
+++ app/routes/settings.js
@@ -1,7 +1,19 @@
 import LeosacRoute from 'web/leosac-route';
+import Ember from 'ember';
 
 export default LeosacRoute.extend({
     authSrv: Ember.inject.service('authentication'),
     leosacInfo: Ember.inject.service('leosac-info'),
-    _title: 'settings'
+    _title: 'settings',
+    _requireAuth: true,
+
+    beforeModel()
+    {
+        "use strict";
+        return this._super();
+    },
+    model()
+    {
+        "use strict";
+    }
 });
