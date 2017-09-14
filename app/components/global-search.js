237470d649498046dc62ae45b39d443f19aef738
marin <marin.brunel@epitech.eu>
2017-09-14 18:17:53 +0200

    There is some progress on the globalSearch, we cant navigate yet, but this is for the next commit

--- app/components/global-search.js
+++ app/components/global-search.js
@@ -1,79 +1,157 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-    // init() {
-    //     this._super(...arguments);
-    //
-    //     const ws = this.get('websocket');
-    //     console.log("la page est chargé");
-    //     console.log(this.get('store'));
-    //
-    //     let store =  new Ember.RSVP.Promise(function (resolve, reject)
-    //         {
-    //           ws.sendJson('users',{}).then((data) => resolve(data),
-    //               (failure) => reject(failure));
-    //         });
-    //     console.log("Le store est remplis");
-    //     console.log(store);
-    // },
+function mySortByTypeThenName(a, b) {
+    let typeA = a.type;
+    let typeB = b.type;
 
+    if (typeA === typeB)
+    {
+        let x = a.content.toLowerCase(), y = b.content.toLowerCase();
+
+        return x < y ? -1 : x > y ? 1 : 0;
+    }
+    return typeA < typeB ? -1 : typeA > typeB ? 1 : 0;
+}
+
+export default Ember.Component.extend({
     globalInfo: Ember.inject.service('leosac-info'),
+    search: Ember.inject.service('search'),
     flashMessages: Ember.inject.service(),
     i18n: Ember.inject.service(),
-    store: Ember.inject.service('store'),
+    store: Ember.inject.service(),
 
     dataToObject: {},
+    groupedNumbers: [
+        { groupName: 'user', options: [] },
+        { groupName: 'zones', options: [] },
+        { groupName: 'doors', options: [] },
+        { groupName: 'groups', options: [] },
+        { groupName: 'pincode', options: [] },
+        { groupName: 'credentials.rfid_card.title', options: [] },
+        { groupName: 'schedules', options: [] },
+        ],
     allObjects: [],
     selectedObjects: false,
+    selected: '',
 
-    // didReceiveAttrs()
-    // {
-    //     const self = this;
-    //
-    //     const dataToObject = {};
-    //     const objects = [];
-    //     console.log(this.get('store'));
-    //     this.get('store').forEach(function(type) {
-    //        console.log(type);
-    //     });
-    //     // this.get('store').findAll('user', {reload: true}).then((users) =>
-    //     // {
-    //     //     users.forEach((user) =>
-    //     //     {
-    //     //         objects.push(user.get('username'));
-    //     //         dataToObject[user.get('username')] = user;
-    //     //     });
-    //     //     self.set('allUsernames', objects);
-    //     //     self.set('usernameToObject', dataToObject);
-    //     // });
-    // },
-    // actions: {
-    //     addToGroup()
-    //     {
-    //         const store = this.get('store');
-    //         const fm = this.get('flashMessages');
-    //
-    //         const user = this.get('usernameToObject')[this.get('selectedUsername')];
-    //         if (!user)
-    //         {
-    //             fm.danger(this.get('i18n').t('users.error.find_error'));
-    //             return;
-    //         }
-    //
-    //         const membership = store.createRecord('user-group-membership');
-    //         membership.set('group', this.get('group'));
-    //         membership.set('rank', this.get('selectedRank'));
-    //         membership.set('user', user);
-    //
-    //         membership.save().then(() =>
-    //             {
-    //                 fm.success(this.get('i18n').t('users.error.add_success'));
-    //             },
-    //             () =>
-    //             {
-    //                 fm.danger(this.get('i18n').t('users.error.add_error'));
-    //                 membership.deleteRecord();
-    //             });
-    //     }
-    // }
+    init() {
+        this._super(...arguments);
+    },
+    didReceiveAttrs()
+    {
+        // const content = [];
+        //
+        // this.get('store').findAll('user', {reload: true}).then((objects) =>
+        // {
+        //     objects.forEach((object) =>
+        //     {
+        //         let typeObject = {
+        //             type: 'user',
+        //             id: '',
+        //             content: ''
+        //         };
+        //         typeObject.id = object.get('id');
+        //         typeObject.content = object.get('username');
+        //         content.push(typeObject);
+        //     });
+        //     this.get('store').findAll('zone', {reload: true}).then((objects) =>
+        //     {
+        //         objects.forEach((object) =>
+        //         {
+        //             let typeObject = {
+        //                 type: 'zones',
+        //                 id: '',
+        //                 content: ''
+        //             };
+        //             typeObject.id = object.get('id');
+        //             typeObject.content = object.get('alias');
+        //             content.push(typeObject);
+        //         });
+        //         this.get('store').findAll('door', {reload: true}).then((objects) =>
+        //         {
+        //             objects.forEach((object) =>
+        //             {
+        //                 let typeObject = {
+        //                     type: 'doors',
+        //                     id: '',
+        //                     content: ''
+        //                 };
+        //                 typeObject.id = object.get('id');
+        //                 typeObject.content = object.get('alias');
+        //                 content.push(typeObject);
+        //             });
+        //             this.get('store').findAll('group', {reload: true}).then((objects) =>
+        //             {
+        //                 objects.forEach((object) =>
+        //                 {
+        //                     let typeObject = {
+        //                         type: 'groups',
+        //                         id: '',
+        //                         content: ''
+        //                     };
+        //                     typeObject.id = object.get('id');
+        //                     typeObject.content = object.get('name');
+        //                     content.push(typeObject);
+        //                 });
+        //                 this.get('store').findAll('pin-code', {reload: true}).then((objects) =>
+        //                 {
+        //                     objects.forEach((object) =>
+        //                     {
+        //                         let typeObject = {
+        //                             type: 'pincode',
+        //                             id: '',
+        //                             content: ''
+        //                         };
+        //                         typeObject.id = object.get('id');
+        //                         typeObject.content = object.get('alias');
+        //                         content.push(typeObject);
+        //                     });
+        //                     this.get('store').findAll('rfid-card', {reload: true}).then((objects) =>
+        //                     {
+        //                         objects.forEach((object) =>
+        //                         {
+        //                             let typeObject = {
+        //                                 type: 'credentials.rfid_card.title',
+        //                                 id: '',
+        //                                 content: ''
+        //                             };
+        //                             typeObject.id = object.get('id');
+        //                             typeObject.content = object.get('alias');
+        //                             content.push(typeObject);
+        //                         });
+        //                         this.get('store').findAll('schedule', {reload: true}).then((objects) =>
+        //                         {
+        //                             objects.forEach((object) =>
+        //                             {
+        //                                 let typeObject = {
+        //                                     type: 'schedules',
+        //                                     id: '',
+        //                                     content: ''
+        //                                 };
+        //                                 typeObject.id = object.get('id');
+        //                                 typeObject.content = object.get('name');
+        //                                 content.push(typeObject);
+        //                             });
+        //                             content.sort(function(a, b) {
+        //                                 return mySortByTypeThenName(a,b);
+        //                             });
+        //                             this.set('allObjects', content);
+        //                         });
+        //                     });
+        //                 });
+        //             });
+        //         });
+        //     });
+        // });
+    },
+    actions:
+        {
+            findAll(partialAlias)
+            {
+                this.get('search').findAllByAlias(partialAlias).then((data) => {
+                    console.log(data);
+                });
+                return this.get('search').findAllByAlias(partialAlias);
+            }
+        }
 });
--- app/services/search.js
+++ app/services/search.js
@@ -16,11 +16,9 @@ export default Ember.Service.extend({
      * Returns a promise that resolves to an array of {id,name}
      * for each group that matched.
      */
-    findGroupByName(partialName)
-    {
+    findGroupByName(partialName) {
         const ws = this.get('websocket');
-        return new Ember.RSVP.Promise(function (resolve, reject)
-        {
+        return new Ember.RSVP.Promise(function (resolve, reject) {
             console.log('searching for ' + partialName);
             ws.sendJson('search.group_name',
                 {
@@ -34,11 +32,9 @@ export default Ember.Service.extend({
      * Returns a promise that resolves to an array of {id,alias}
      * for each door that matched.
      */
-    findDoorByAlias(partialName)
-    {
+    findDoorByAlias(partialName) {
         const ws = this.get('websocket');
-        return new Ember.RSVP.Promise(function (resolve, reject)
-        {
+        return new Ember.RSVP.Promise(function (resolve, reject) {
             console.log('searching for ' + partialName);
             ws.sendJson('search.door_alias',
                 {
@@ -51,11 +47,9 @@ export default Ember.Service.extend({
     /**
      * Doesn't work at all, need the backend of this aka
      */
-    findZoneByAlias(partialName)
-    {
+    findZoneByAlias(partialName) {
         const ws = this.get('websocket');
-        return new Ember.RSVP.Promise(function (resolve, reject)
-        {
+        return new Ember.RSVP.Promise(function (resolve, reject) {
             console.log('searching for ' + partialName);
             ws.sendJson('search.zone_alias',
                 {
@@ -65,43 +59,80 @@ export default Ember.Service.extend({
         });
     },
 
-    /**
-     * Returns a list of enabled AccessPoint modules.
-     * Currently this list is hardcoded client side.
-     */
-    listAccessPointModuleNames()
-    {
-        return ['EVOXS'];
-    },
-
-    /**
-     * @see findDoorByAlias.
-     */
-    findAccessPointByAlias(partialName)
-    {
+    findAllByAlias(partialName) {
         const ws = this.get('websocket');
-        return new Ember.RSVP.Promise(function (resolve, reject)
-        {
-            console.log('searching for ' + partialName);
-            ws.sendJson('search.access_point_alias',
-                {
-                    'partial_name': partialName
-                }).then((data) => resolve(data),
-                (failure) => reject(failure));
+        let resultSearch = [];
+
+        let p1 = this.findZoneByAlias(partialName);
+        let p2 = this.findDoorByAlias(partialName);
+        let p3 = this.findGroupByName(partialName);
+        return new Ember.RSVP.Promise(function (resolve, reject) {
+            Ember.RSVP.all([p1, p2, p3]).then((data) => {
+                data[0].forEach(function (zone) {
+                    resultSearch.push({
+                        type: 'zone',
+                        id: zone.id,
+                        nameOrAlias: zone.alias
+                    });
+                });
+                data[1].forEach(function (door) {
+                    resultSearch.push({
+                        type: 'door',
+                        id: door.id,
+                        nameOrAlias: door.alias
+                    });
+                });
+                data[2].forEach(function (group) {
+                    resultSearch.push({
+                        type: 'group',
+                        id: group.id,
+                        nameOrAlias: group.name
+                    });
+                });
+                resolve(resultSearch);
+            });
         });
     },
 
-    findScheduleByName(partialName)
-    {
-        const ws = this.get('websocket');
-        return new Ember.RSVP.Promise(function (resolve, reject)
-        {
-            console.log('searching for ' + partialName);
-            ws.sendJson('search.schedule_name',
-                {
-                    'partial_name': partialName
-                }).then((data) => resolve(data),
-                (failure) => reject(failure));
-        });
-    },
-});
+    /**
+ * Returns a list of enabled AccessPoint modules.
+ * Currently this list is hardcoded client side.
+ */
+listAccessPointModuleNames()
+{
+    return ['EVOXS'];
+}
+,
+
+/**
+ * @see findDoorByAlias.
+ */
+findAccessPointByAlias(partialName)
+{
+    const ws = this.get('websocket');
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+        console.log('searching for ' + partialName);
+        ws.sendJson('search.access_point_alias',
+            {
+                'partial_name': partialName
+            }).then((data) => resolve(data),
+            (failure) => reject(failure));
+    });
+}
+,
+
+findScheduleByName(partialName)
+{
+    const ws = this.get('websocket');
+    return new Ember.RSVP.Promise(function (resolve, reject) {
+        console.log('searching for ' + partialName);
+        ws.sendJson('search.schedule_name',
+            {
+                'partial_name': partialName
+            }).then((data) => resolve(data),
+            (failure) => reject(failure));
+    });
+}
+,
+})
+;
