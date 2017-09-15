e1e2d7cc9135af50ecb0ed8edc5291b2cdeb43d8
marin <marin.brunel@epitech.eu>
2017-09-15 17:13:18 +0200

    The components global search is almost finish. The template can be more user friendly. We wait to implement the credentials for the next update or the backend leosac

--- app/components/global-search.js
+++ app/components/global-search.js
@@ -1,18 +1,5 @@
 import Ember from 'ember';
 
-function mySortByTypeThenName(a, b) {
-    let typeA = a.type;
-    let typeB = b.type;
-
-    if (typeA === typeB)
-    {
-        let x = a.content.toLowerCase(), y = b.content.toLowerCase();
-
-        return x < y ? -1 : x > y ? 1 : 0;
-    }
-    return typeA < typeB ? -1 : typeA > typeB ? 1 : 0;
-}
-
 export default Ember.Component.extend({
     globalInfo: Ember.inject.service('leosac-info'),
     search: Ember.inject.service('search'),
@@ -21,137 +8,145 @@ export default Ember.Component.extend({
     store: Ember.inject.service(),
 
     dataToObject: {},
-    groupedNumbers: [
-        { groupName: 'user', options: [] },
-        { groupName: 'zones', options: [] },
-        { groupName: 'doors', options: [] },
-        { groupName: 'groups', options: [] },
-        { groupName: 'pincode', options: [] },
-        { groupName: 'credentials.rfid_card.title', options: [] },
-        { groupName: 'schedules', options: [] },
-        ],
     allObjects: [],
     selectedObjects: false,
+    selectedZone: '',
     selected: '',
 
     init() {
         this._super(...arguments);
     },
-    didReceiveAttrs()
-    {
-        // const content = [];
-        //
-        // this.get('store').findAll('user', {reload: true}).then((objects) =>
-        // {
-        //     objects.forEach((object) =>
-        //     {
-        //         let typeObject = {
-        //             type: 'user',
-        //             id: '',
-        //             content: ''
-        //         };
-        //         typeObject.id = object.get('id');
-        //         typeObject.content = object.get('username');
-        //         content.push(typeObject);
-        //     });
-        //     this.get('store').findAll('zone', {reload: true}).then((objects) =>
-        //     {
-        //         objects.forEach((object) =>
-        //         {
-        //             let typeObject = {
-        //                 type: 'zones',
-        //                 id: '',
-        //                 content: ''
-        //             };
-        //             typeObject.id = object.get('id');
-        //             typeObject.content = object.get('alias');
-        //             content.push(typeObject);
-        //         });
-        //         this.get('store').findAll('door', {reload: true}).then((objects) =>
-        //         {
-        //             objects.forEach((object) =>
-        //             {
-        //                 let typeObject = {
-        //                     type: 'doors',
-        //                     id: '',
-        //                     content: ''
-        //                 };
-        //                 typeObject.id = object.get('id');
-        //                 typeObject.content = object.get('alias');
-        //                 content.push(typeObject);
-        //             });
-        //             this.get('store').findAll('group', {reload: true}).then((objects) =>
-        //             {
-        //                 objects.forEach((object) =>
-        //                 {
-        //                     let typeObject = {
-        //                         type: 'groups',
-        //                         id: '',
-        //                         content: ''
-        //                     };
-        //                     typeObject.id = object.get('id');
-        //                     typeObject.content = object.get('name');
-        //                     content.push(typeObject);
-        //                 });
-        //                 this.get('store').findAll('pin-code', {reload: true}).then((objects) =>
-        //                 {
-        //                     objects.forEach((object) =>
-        //                     {
-        //                         let typeObject = {
-        //                             type: 'pincode',
-        //                             id: '',
-        //                             content: ''
-        //                         };
-        //                         typeObject.id = object.get('id');
-        //                         typeObject.content = object.get('alias');
-        //                         content.push(typeObject);
-        //                     });
-        //                     this.get('store').findAll('rfid-card', {reload: true}).then((objects) =>
-        //                     {
-        //                         objects.forEach((object) =>
-        //                         {
-        //                             let typeObject = {
-        //                                 type: 'credentials.rfid_card.title',
-        //                                 id: '',
-        //                                 content: ''
-        //                             };
-        //                             typeObject.id = object.get('id');
-        //                             typeObject.content = object.get('alias');
-        //                             content.push(typeObject);
-        //                         });
-        //                         this.get('store').findAll('schedule', {reload: true}).then((objects) =>
-        //                         {
-        //                             objects.forEach((object) =>
-        //                             {
-        //                                 let typeObject = {
-        //                                     type: 'schedules',
-        //                                     id: '',
-        //                                     content: ''
-        //                                 };
-        //                                 typeObject.id = object.get('id');
-        //                                 typeObject.content = object.get('name');
-        //                                 content.push(typeObject);
-        //                             });
-        //                             content.sort(function(a, b) {
-        //                                 return mySortByTypeThenName(a,b);
-        //                             });
-        //                             this.set('allObjects', content);
-        //                         });
-        //                     });
-        //                 });
-        //             });
-        //         });
-        //     });
-        // });
-    },
+
     actions:
         {
+            clear()
+            {
+                this.set('selected', null);
+                this.get('onChange')(null);
+            },
+            goToSelected() {
+                if (this.get('selected.type') === 'typeUser')
+                    window.location = '/profile/' + this.get('selected.id');
+                else if (this.get('selected.type') === 'typeZone')
+                    window.location = '/zone/' + this.get('selected.id');
+                else if (this.get('selected.type') === 'typeDoor')
+                    window.location = '/door/' + this.get('selected.id');
+                else if (this.get('selected.type') === 'typeSchedule')
+                    window.location = '/schedule/' + this.get('selected.id');
+                else if (this.get('selected.type') === 'typeGroup')
+                    window.location = '/group/' + this.get('selected.id');
+                else if (this.get('selected.type') === 'typePinCode')
+                    window.location = '/credentials/pin-code/' + this.get('selected.id');
+                else if (this.get('selected.type') === 'typeRfidCard')
+                    window.location = '/credentials/rfid-card/' + this.get('selected.id');
+            },
+            /**
+             * There is some tricky things happening in this function:
+             *
+             * First, we have to return an object different to the promise to sort it for example.
+             *
+             * Then, we have a specificName and name for each elements,
+             * because every model has a different name,
+             * but we need something clean for the templates.
+             *
+             * Then, we sort our object by type, and then by name
+             *
+             * Finally we return this filled and sorted object to ember-power-select-typeahead
+             */
             findAll(partialAlias)
             {
-                this.get('search').findAllByAlias(partialAlias).then((data) => {
-                    console.log(data);
+                return this.get('search').findAllByAlias(partialAlias).then((data) => {
+                    let resultObject = [];
+
+                    data.forEach(function(objects) {
+                        let typeObject = {};
+
+                        if (objects.type === 'user')
+                        {
+                            typeObject = {
+                                username: objects.nameOrAlias,
+                                name:  objects.nameOrAlias,
+                                type: 'typeUser',
+                                id: objects.id
+                            };
+                        }
+                        else if (objects.type === 'zone')
+                        {
+                            typeObject = {
+                                alias: objects.nameOrAlias,
+                                name:  objects.nameOrAlias,
+                                type: 'typeZone',
+                                id: objects.id
+                            };
+                        }
+                        else if (objects.type === 'door')
+                        {
+                            typeObject = {
+                                alias: objects.nameOrAlias,
+                                name:  objects.nameOrAlias,
+                                type: 'typeDoor',
+                                id: objects.id
+                            };
+                        }
+                        else if (objects.type === 'schedule')
+                        {
+                            typeObject = {
+                                name: objects.nameOrAlias,
+                                type: 'typeSchedule',
+                                id: objects.id
+                            };
+                        }
+                        else if (objects.type === 'group')
+                        {
+                            typeObject = {
+                                name:  objects.nameOrAlias,
+                                type: 'typeGroup',
+                                id: objects.id
+                            };
+                        }
+                        else if (objects.type === 'pincode')
+                        {
+                            typeObject = {
+                                alias:  objects.nameOrAlias,
+                                type: 'typePinCode',
+                                id: objects.id
+                            };
+                        }
+                        else if (objects.type === 'rfidcard')
+                        {
+                            typeObject = {
+                                alias:  objects.nameOrAlias,
+                                type: 'typeRfidCard',
+                                id: objects.id
+                            };
+                        }
+                        resultObject.push(typeObject);
+                    });
+                    resultObject.sort(function(a, b) {
+                        let typeA = a.type.toLowerCase();
+                        let typeB = b.type.toLowerCase();
+
+                        if (typeA === typeB)
+                        {
+                            let nameA = a.name.toLowerCase();
+                            let nameB = b.name.toLowerCase();
+
+                            return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+                        }
+                        return typeA < typeB ? -1 : typeA > typeB ? 1 : 0;
+                    });
+                    return resultObject;
                 });
-                return this.get('search').findAllByAlias(partialAlias);
             }
         }
 });
+
+/**
+ { groupName: 'user', options: [] },
+ { groupName: 'zone.title', options: [] },
+ { groupName: 'door.title', options: [] },
+ { groupName: 'group.title', options: [] },
+ { groupName: 'pincode', options: [] },
+ { groupName: 'credentials.rfid_card.title', options: [] },
+ { groupName: 'schedule.title', options: [] },
+ */
--- app/services/search.js
+++ app/services/search.js
@@ -18,9 +18,26 @@ export default Ember.Service.extend({
      */
     findGroupByName(partialName) {
         const ws = this.get('websocket');
+
         return new Ember.RSVP.Promise(function (resolve, reject) {
-            console.log('searching for ' + partialName);
             ws.sendJson('search.group_name',
+                {
+                    'partial_name': partialName
+                }).then((data) => resolve(data),
+                (failure) => reject(failure));
+        });
+    },
+
+    /**
+     * Returns a promise that resolves to an array of {id,name}
+     * for each schedule that matched.
+     */
+    findScheduleByName(partialName)
+    {
+        const ws = this.get('websocket');
+
+        return new Ember.RSVP.Promise(function (resolve, reject) {
+            ws.sendJson('search.schedule_name',
                 {
                     'partial_name': partialName
                 }).then((data) => resolve(data),
@@ -34,8 +51,8 @@ export default Ember.Service.extend({
      */
     findDoorByAlias(partialName) {
         const ws = this.get('websocket');
+
         return new Ember.RSVP.Promise(function (resolve, reject) {
-            console.log('searching for ' + partialName);
             ws.sendJson('search.door_alias',
                 {
                     'partial_name': partialName
@@ -45,12 +62,29 @@ export default Ember.Service.extend({
     },
 
     /**
-     * Doesn't work at all, need the backend of this aka
+     * Return a promise that resolve to an array of {id, name}
+     * for each user that matched
+     */
+    findUserByUsername(partialName) {
+        const ws = this.get('websocket');
+
+        return new Ember.RSVP.Promise(function (resolve, reject) {
+            ws.sendJson('search.user_username',
+                {
+                    'partial_name': partialName
+                }).then((data) => resolve(data),
+                (failure) => reject(failure));
+        });
+    },
+
+    /**
+     * Return a promise that resolve to an array of {id, alias}
+     * for each zone that matched
      */
     findZoneByAlias(partialName) {
-        const ws = this.get('websocket');
+        const ws = this.get('websocket')
+
         return new Ember.RSVP.Promise(function (resolve, reject) {
-            console.log('searching for ' + partialName);
             ws.sendJson('search.zone_alias',
                 {
                     'partial_name': partialName
@@ -59,80 +93,124 @@ export default Ember.Service.extend({
         });
     },
 
+    /**
+     * Return a promise that resolve to an array of {type, id, nameOrAlias}
+     * for each zone, door, group, schedule and user in our DB that matched
+     *
+     * Firstly, we call all our promise that need to be resolved
+     *
+     * Then we use Promise all() that will wait that all our promise are resolved,
+     *
+     * And  then we fill the object that will be sent to the application
+     */
     findAllByAlias(partialName) {
-        const ws = this.get('websocket');
         let resultSearch = [];
 
-        let p1 = this.findZoneByAlias(partialName);
-        let p2 = this.findDoorByAlias(partialName);
-        let p3 = this.findGroupByName(partialName);
+        let promiseZone = this.findZoneByAlias(partialName);
+        let promiseDoor = this.findDoorByAlias(partialName);
+        let promiseGroup = this.findGroupByName(partialName);
+        let promiseSchedule = this.findScheduleByName(partialName);
+        let promiseUser = this.findUserByUsername(partialName);
+//        let promiseCredentiall = this.findCredentialByAlias(partialName);
+
         return new Ember.RSVP.Promise(function (resolve, reject) {
-            Ember.RSVP.all([p1, p2, p3]).then((data) => {
-                data[0].forEach(function (zone) {
-                    resultSearch.push({
-                        type: 'zone',
-                        id: zone.id,
-                        nameOrAlias: zone.alias
+            Ember.RSVP.all([promiseZone,
+                promiseDoor,
+                promiseGroup,
+                promiseSchedule,
+                promiseUser
+            ]).then((data) => {
+                if (data[0]) {
+                    data[0].forEach(function (zone) {
+                        resultSearch.push({
+                            type: 'zone',
+                            id: zone.id,
+                            nameOrAlias: zone.alias
+                        });
                     });
-                });
-                data[1].forEach(function (door) {
-                    resultSearch.push({
-                        type: 'door',
-                        id: door.id,
-                        nameOrAlias: door.alias
+                }
+                if (data[1]) {
+                    data[1].forEach(function (door) {
+                        resultSearch.push({
+                            type: 'door',
+                            id: door.id,
+                            nameOrAlias: door.alias
+                        });
                     });
-                });
-                data[2].forEach(function (group) {
-                    resultSearch.push({
-                        type: 'group',
-                        id: group.id,
-                        nameOrAlias: group.name
+                }
+                if (data[2]) {
+                    data[2].forEach(function (group) {
+                        resultSearch.push({
+                            type: 'group',
+                            id: group.id,
+                            nameOrAlias: group.name
+                        });
                     });
-                });
+                }
+                if (data[3]) {
+                    data[3].forEach(function (schedule) {
+                        resultSearch.push({
+                            type: 'schedule',
+                            id: schedule.id,
+                            nameOrAlias: schedule.name
+                        });
+                    });
+                }
+                if (data[4]) {
+                    data[4].forEach(function (user) {
+                        resultSearch.push({
+                            type: 'user',
+                            id: user.id,
+                            nameOrAlias: user.username
+                        });
+                    });
+                }
+                if (data[5]) {
+                    data[5].forEach(function (credentials) {
+                        if (credentials.type === 'pincode')
+                            resultSearch.push({
+                                type: 'pincode',
+                                id: pincode.id,
+                                nameOrAlias: pincode.alias
+                            });
+                        else
+                            resultSearch.push({
+                                type: 'rfidcard',
+                                id: rfidcard.id,
+                                nameOrAlias: rfidcard.alias
+                            });
+                    });
+                }
                 resolve(resultSearch);
             });
         });
     },
 
     /**
- * Returns a list of enabled AccessPoint modules.
- * Currently this list is hardcoded client side.
- */
-listAccessPointModuleNames()
-{
-    return ['EVOXS'];
-}
-,
+     * Returns a list of enabled AccessPoint modules.
+     * Currently this list is hardcoded client side.
+     */
+    listAccessPointModuleNames()
+    {
+        return ['EVOXS'];
+    }
+    ,
 
-/**
- * @see findDoorByAlias.
- */
-findAccessPointByAlias(partialName)
-{
-    const ws = this.get('websocket');
-    return new Ember.RSVP.Promise(function (resolve, reject) {
-        console.log('searching for ' + partialName);
-        ws.sendJson('search.access_point_alias',
-            {
-                'partial_name': partialName
-            }).then((data) => resolve(data),
-            (failure) => reject(failure));
-    });
-}
-,
+    /**
+     * @see findDoorByAlias.
+     */
+    findAccessPointByAlias(partialName)
+    {
+        const ws = this.get('websocket');
 
-findScheduleByName(partialName)
-{
-    const ws = this.get('websocket');
-    return new Ember.RSVP.Promise(function (resolve, reject) {
-        console.log('searching for ' + partialName);
-        ws.sendJson('search.schedule_name',
-            {
-                'partial_name': partialName
-            }).then((data) => resolve(data),
-            (failure) => reject(failure));
-    });
-}
-,
+        return new Ember.RSVP.Promise(function (resolve, reject) {
+            console.log('searching for ' + partialName);
+            ws.sendJson('search.access_point_alias',
+                {
+                    'partial_name': partialName
+                }).then((data) => resolve(data),
+                (failure) => reject(failure));
+        });
+    },
 })
 ;
