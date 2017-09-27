38398d6b27e29987a28aa3eaab7c0559fa4da1fb
marin <marin.brunel@epitech.eu>
2017-09-27 14:37:00 +0200

    Added a reconnection view in the footer. If the server is connected, there is now a green check in the footer, otherwise there is a red spinner.

--- app/components/my-footer.js
+++ app/components/my-footer.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  globalInfo: Ember.inject.service('leosac-info'),
-  authSrv: Ember.inject.service('authentication')
+    globalInfo: Ember.inject.service('leosac-info'),
+    ws: Ember.inject.service('websocket'),
+
+    init() {
+        this._super(...arguments);
+    },
+
 });
