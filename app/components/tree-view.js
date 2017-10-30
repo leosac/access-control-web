47e4ec50cb27dea441f26d25f3b63ddf31c92107
Marin Brunel <marin.brunel@epitech.eu>
2017-10-30 17:09:04 +0100

    Removed the old summary display, putted it in the style.css .

--- app/components/tree-view.js
+++ app/components/tree-view.js
@@ -89,7 +89,7 @@ export default Ember.Component.extend({
             "icon": "fa fa-building"
         },
         "logical": {
-            "icon": "fa fa-users"
+            "icon": "fa fa-puzzle-piece"
         },
         "physicalZone": {
             "icon": "fa fa-building-o"
