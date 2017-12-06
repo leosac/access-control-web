fbf74c44cabd4e2274285ddfe6b569532c1d1e6b
Marin Brunel <marin.brunel@epitech.eu>
2017-12-06 11:21:28 +0100

    Added a mixins that will allow us to properly save the schedule, before that, when you added a schedule-mapping and saved it, a duplicate will be shown.

---
+++ app/mixins/dependent-relationships.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export default Ember.Mixin.create({
+    /**
+     * This is the save method for the schedule mapping
+     * There is an issue related to it on github: https://github.com/emberjs/data/issues/1829
+     *
+     * This is related with ember data and an Has Many relationships in a model.
+     *
+     * It will put every part of the relationships in an array,
+     * if there is a duplicate, it will delete it,
+     * and then it will save the record.
+     *
+     * @returns {promise} This return a promise of the save method.
+     */
+    save: function () {
+        let recordsToDelete = [];
+        this.eachRelationship((name, descriptor) => {
+            if (descriptor.kind === 'hasMany') {
+                this.get(name).forEach((object) => {
+                    if (object.get('isNew')) {
+                        recordsToDelete.push(object);
+                    }
+                });
+            }
+        });
+
+        let promise = this._super(...arguments);
+        promise.then(() => {
+            recordsToDelete.forEach((record) => {
+                record.deleteRecord();
+            });
+        });
+        return promise;
+    }
+});
