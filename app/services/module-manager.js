8518fb2d9742ca639ee6bb2e5707a73ecadb5585
Marin Brunel <marin.brunel@epitech.eu>
2017-12-11 11:54:37 +0100

    Removed some bits in the module-manager.

--- app/services/module-manager.js
+++ app/services/module-manager.js
@@ -1,14 +1,7 @@
 import Ember from 'ember';
 
-function camelize(str) {
-    return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
-        if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
-        return index === 0 ? match.toLowerCase() : match.toUpperCase();
-    });
-}
-
 /**
- * This service will help us manage every module that are in our application,
+ * This function will help us manage every module that are in our application,
  */
 
 function formatName(object) {
