9e919d0914b96a61dd743ae16afcf9cad0dcc4ed
Marin Brunel <marin.brunel@epitech.eu>
2017-12-11 15:30:22 +0100

    Added the ember-cli-eslint module. It will show the us a message when there is something wrong about the variables of our application for example. If there is an error, like an unused variable, it will tell us.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,17 @@
+/*eslint-env node*/
 module.exports = {
-  root: true,
-  parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module'
-  },
-  extends: 'eslint:recommended',
-  env: {
-    browser: true
-  },
-  rules: {
-  }
+    root: true,
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module'
+    },
+    extends: 'eslint:recommended',
+    env: {
+        "es6": true,
+        "jquery": true,
+        browser: true
+    },
+    rules: {
+        "no-console": "off"
+    },
 };
