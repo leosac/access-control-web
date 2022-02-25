26e6d52080684549e12ced443c874a37b4561e22
Maxhy <maxime@islog.com>
2022-02-25 11:03:25 +0100

    Fix merge

--- app/router.js
+++ app/router.js
@@ -4,6 +4,7 @@ import config from './config/environment';
 const Router = Ember.Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
+});
 
 Router.map(function () {
   this.route('index', {path: '/'});
