36eca86aac4e00ab39e21c007164bbb2a7a2c89f
Maxime C. <maxime@leosac.com>
2025-08-25 18:12:09 +0200

    Update ember-cli-jstree to a temporary updated version

--- app/routes/credentials/pin-code.js
+++ app/routes/credentials/pin-code.js
@@ -2,9 +2,7 @@ import { inject as service } from '@ember/service';
 import LeosacRoute from 'web/leosac-route';
 
 export default LeosacRoute.extend({
-    router: service(),
     store: service(),
-    flashMessages: service(),
     _title: 'credentials.pin_code.title',
     _requireAuth: true,
     beforeModel()
--- app/routes/credentials/rfid-card.js
+++ app/routes/credentials/rfid-card.js
@@ -2,9 +2,7 @@ import { inject as service } from '@ember/service';
 import LeosacRoute from 'web/leosac-route';
 
 export default LeosacRoute.extend({
-    router: service(),
     store: service(),
-    flashMessages: service(),
     _title: 'credentials.rfid_card.title',
     _requireAuth: true,
     beforeModel()
