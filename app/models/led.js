f6172f786a80c168e2169c01c64edb8bfba48151
Maxime C. <maxime@leosac.com>
2025-08-28 22:53:06 +0200

    Temporary fix for add-ons models to be available on Store

---
+++ app/models/led.js
@@ -0,0 +1,1 @@
+export { default } from 'led-buzzer/models/led';
---
+++ app/models/piface-digital-gpio.js
@@ -0,0 +1,2 @@
+//TODO: remove this. It should only be on the add-on /app/models folder instead
+export { default } from 'piface-digital-gpio/models/piface-digital-gpio';
